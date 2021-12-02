370b24c31ac831fbfb3345f43449fdac3bf9766f
Saige Cross <saigecrossdev@gmail.com>
2021-12-01 19:42:12 -0600

    added dash card

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react'
-import { Button, Alert } from 'react-bootstrap'
+import { Button, Alert, Card } from 'react-bootstrap'
+import Timer from '../components/Timer'
 import Navbar from './Navbar'
 import  '../App.css'  
-
-
 
 export default function Dashboard() {
   
@@ -11,7 +10,15 @@ export default function Dashboard() {
   return (
     <div>
       <Navbar />
-      ALLO, DASHBOARD
+      <div className="dashcontainer">
+        <Card className="text-center" style={{ width: '22rem' }}>
+          <Card.Body className="shadow sm">
+            <Card.Title >Reading Timer</Card.Title>
+            <Timer />
+          </Card.Body>
+        </Card>
+      </div>
     </div>
+
   )
 }
