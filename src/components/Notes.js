b24eb0b71be9cec5169877f3837d9ba2ab224d35
Saige Cross <saigecrossdev@gmail.com>
2022-01-20 20:21:00 -0600

    Added TypeScript, typing /Notes.tsx (2 errors left)

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-
-export default function App() {
-  const [todos, setTodos] = useState(() => {
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const react_1 = require('react');
+const react_bootstrap_1 = require('react-bootstrap');
+const react_fontawesome_1 = require('@fortawesome/react-fontawesome');
+const free_solid_svg_icons_1 = require('@fortawesome/free-solid-svg-icons');
+function App() {
+  const [todos, setTodos] = (0, react_1.useState)(() => {
     const savedTodos = localStorage.getItem('todos');
     if (savedTodos) {
       return JSON.parse(savedTodos);
@@ -12,28 +13,22 @@ export default function App() {
       return [];
     }
   });
-  const [todo, setTodo] = useState('');
-  const [inputvalue, setInputValue] = useState('');
-
+  const [todo, setTodo] = (0, react_1.useState)('');
+  const [inputvalue, setInputValue] = (0, react_1.useState)('');
   function handleUserInput(e) {
     setInputValue(e.target.value);
   }
-
   function resetInputField() {
     setInputValue('');
   }
-
-  useEffect(() => {
+  (0, react_1.useEffect)(() => {
     localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
-
   function handleInputChange(e) {
     setTodo(e.target.value);
   }
-
   function handleFormSubmit(e) {
     e.preventDefault();
-
     if (todo !== '') {
       setTodos([
         ...todos,
@@ -44,34 +39,37 @@ export default function App() {
       ]);
     }
   }
-
   function handleDeleteClick(id) {
     const removeItem = todos.filter((todo) => {
       return todo.id !== id;
     });
     setTodos(removeItem);
   }
-
   return (
     <div>
-      <Form
+      <react_bootstrap_1.Form
         onSubmit={handleFormSubmit}
         value={todo}
         onChange={handleInputChange}
       >
-        <Form.Group
+        <react_bootstrap_1.Form.Group
           className="mb-3"
           controlId="exampleForm.ControlInput1"
-        ></Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Thoughts & Things:</Form.Label>
-          <Form.Control
+        ></react_bootstrap_1.Form.Group>
+        <react_bootstrap_1.Form.Group
+          className="mb-3"
+          controlId="exampleForm.ControlTextarea1"
+        >
+          <react_bootstrap_1.Form.Label>
+            Thoughts & Things:
+          </react_bootstrap_1.Form.Label>
+          <react_bootstrap_1.Form.Control
             as="textarea"
             value={inputvalue}
             onChange={handleUserInput}
             rows={3}
-          ></Form.Control>
-          <Button
+          ></react_bootstrap_1.Form.Control>
+          <react_bootstrap_1.Button
             style={{ backgroundColor: '#97D9E1', border: 'none' }}
             onClick={resetInputField}
             className="mt-2"
@@ -79,24 +77,27 @@ export default function App() {
             type="submit"
           >
             <strong>Submit</strong>
-          </Button>
-        </Form.Group>
-      </Form>
+          </react_bootstrap_1.Button>
+        </react_bootstrap_1.Form.Group>
+      </react_bootstrap_1.Form>
 
       <ul className="todo-list">
         {todos.map((todo) => (
           <li key={todo.id}>
             {todo.text}{' '}
-            <Button
+            <react_bootstrap_1.Button
               variant="link"
               size="sm"
               onClick={() => handleDeleteClick(todo.id)}
             >
-              <FontAwesomeIcon icon={faTrash} />
-            </Button>
+              <react_fontawesome_1.FontAwesomeIcon
+                icon={free_solid_svg_icons_1.faTrash}
+              />
+            </react_bootstrap_1.Button>
           </li>
         ))}
       </ul>
     </div>
   );
 }
+export default App();
