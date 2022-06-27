0203a109fa291461e24a37d9f27765e06e7e0eb3
Saige Cross <saigecrossdev@gmail.com>
2022-06-27 10:20:07 -0500

    added .env at root for port security

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const dotenv = require('dotenv').config()
+const port = process.env.PORT
 const colors = require('colors')
 
 app.get('/', (req, res) => {
