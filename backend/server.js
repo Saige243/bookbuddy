c788b92560c3a8ee0425ead0eb5c23511cdd9334
Saige Cross <saigecrossdev@gmail.com>
2022-06-28 12:30:35 -0500

    installed Mongo, created database, connected to db via express!

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
 const port = process.env.SERVER_PORT
 const colors = require('colors')
+
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('Hola, world!!')
