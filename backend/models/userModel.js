22885504a5c76640f9cc62c651810998622b46c7
Saige Cross <saigecrossdev@gmail.com>
2022-06-29 07:44:47 -0500

    userModel setup, now for userController

---
+++ backend/models/userModel.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password']
+  },
+},
+  {
+    timestamps: true
+  })
+
+module.exports = mongoose.model('User', userSchema)
