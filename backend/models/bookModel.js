2ddcaa3d8446e0c81e9a70b13da83e822488d0e8
Saige Cross <saigecrossdev@gmail.com>
2022-06-30 07:17:14 -0500

    added user to bookSchema

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose'
 
 const bookSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
   title: String,
   author: String,
   thumbnail: String
